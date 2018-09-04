0f5fb3c904507a20d7fee26c6c97dacfd60a7f77
Cody Brocious <cody.brocious@gmail.com>
2018-09-03 22:59:20 -0400

    Added Roslyn compilation to DLL

--- Cli/Program.cs
+++ Cli/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Nettens.CompilerCore;
 
 namespace Nettens.Cli {
@@ -7,7 +10,17 @@ namespace Nettens.Cli {
 		static void Main(string[] args) {
 			var compiler = new Compiler();
 			compiler.LoadIR(File.ReadAllText(args[0]));
-			compiler.Compile(args[1]);
+			compiler.Compile("../tests/temp.cs");
+
+			var tree = SyntaxFactory.ParseSyntaxTree(File.ReadAllText("../tests/temp.cs"));
+			var compilation = CSharpCompilation.Create("NettensOutput")
+				.WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, allowUnsafe: true))
+				.AddReferences(MetadataReference.CreateFromFile(typeof(object).GetTypeInfo().Assembly.Location))
+				.AddSyntaxTrees(tree);
+			var res = compilation.Emit(args[1]);
+			if(!res.Success)
+				foreach(var issue in res.Diagnostics)
+					Console.WriteLine($"ID: {issue.Id} Message: {issue.GetMessage()} Location: {issue.Location.GetLineSpan()} Severity: {issue.Severity}");
 		}
 	}
 }
